1859756fce80f0f3d28bd0e722e3716810b479cb
Allan Ratemo <allanisratemo@gmail.com>
2026-01-16 14:20:30 +0300

    Ready for submission

--- server/app.py
+++ server/app.py
@@ -37,7 +37,7 @@ class CheckSession(Resource):
     def get(self):
         user_id = session.get('user_id')
         if user_id:
-            user = User.query.get(user_id)
+            user = db.session.get(User, user_id)
             if user:
                 return user.to_dict(), 200
         return {'error': 'Unauthorized'}, 401
--- server/models.py
+++ server/models.py
@@ -7,9 +7,11 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
+    serialize_rules = ('-recipes',)
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String, nullable=False)
+    _password_hash = db.Column(db.String, nullable=True)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
